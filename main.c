2ae0d9b8ebd8b1217c9072340660d90a9725a86d
Stanislav <122568647+santasas32@users.noreply.github.com>
2024-02-15 15:26:08 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("\t\t\t            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+		   "\t\t\t           $   wellcome to the c sapper    $\n"
+		   "\t\t\t            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n");
+	printf("choose action:\n"
+		"\t1) play\n\t2) settings\n\t3) exit\n");
+	system("PAUSE");
+	system("cls");
+	return 0;
+}
