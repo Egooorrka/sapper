375f2a0626929b529d494ba3b4f17de80d3a90c2
Stanislav <Stasbyka123@gmail.com>
2024-02-29 15:04:37 +0300

    upd

--- main.c
+++ main.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 int main(){
-	printf("\t\t\t            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
-		   "\t\t\t           $   wellcome to the c sapper    $\n"
-		   "\t\t\t            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n");
-	printf("choose action:\n"
+	printf("\t\t\t            *******************************\n"
+		   "\t\t\t           *   wellcome to the C sapper    *\n"
+		   "\t\t\t            *******************************\n\n");
+
+
+	printf("Choose action:\n"
 		"\t1) play\n\t2) settings\n\t3) exit\n");
 	system("PAUSE");
 	system("cls");
